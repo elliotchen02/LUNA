313ce2631a5b54b3e91c0ca52a6cdbd2e14bf4e3
elliotchen02 <echen02@mit.edu>
2023-08-15 18:06:44 +0800

    Create training.py

---
+++ training.py
@@ -0,0 +1,68 @@
+import torch 
+import torch.nn as nn 
+from torch.optim import SGD, Adam
+from torch.utils.data import DataLoader
+
+import numpy
+import os
+import sys
+import datetime
+import argparse
+
+from utils.utils import enumerateWithEstimate
+from .datasets import LunaDataset
+from .model import LunaModel
+
+
+class LunaTrainingApp:
+    def __init__(self, sys_argv=None) -> None:
+        if sys_argv is None:
+            sys_argv = sys.argv[1:]
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--num-workers',
+            help='Number of worker processes for background data loading',
+            default=8,
+            type=int,
+        )
+        parser.add_argument('--batch-size',
+            help='Batch size to use for training',
+            default=32,
+            type=int,
+        )
+        parser.add_argument('--epochs',
+            help='Number of epochs to train for',
+            default=1,
+            type=int,
+        )
+
+        # parser.add_argument('--tb-prefix',
+        #     default='p2ch11',
+        #     help="Data prefix to use for Tensorboard run. Defaults to chapter.",
+        # )
+
+        # parser.add_argument('comment',
+        #     help="Comment suffix for Tensorboard run.",
+        #     nargs='?',
+        #     default='dwlpt',
+        # )
+
+        self.cli_args = parser.parse_args(sys_argv)
+        self.time_str = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
+
+        self.trn_writer = None
+        self.val_writer = None
+        self.totalTrainingSamples_count = 0
+
+        self.use_cuda = torch.cuda.is_available()
+        self.device = torch.device("cuda" if self.use_cuda else "cpu")
+
+        self.model = self.initModel()
+
+
+        
+
+
+
+
+    
